86c76e650ed3cd685151bfbaeccf9b48bdb417f7
ᯓ꯭𓆰꯭×꯭🔥⁠⁠⁠𝐕꯭𝐀꯭𝐌꯭𝐏꯭𝐈꯭𝐑꯭𝐄꯭ 𝐊꯭𝐈꯭𝐍꯭𝐆 ꯭🌾꯭ <opvampire57@gmail.com>
2025-12-25 19:38:28 +0530

    Simplify admin filter implementation

--- VampireMusic/utils/Sona_BAN.py
+++ VampireMusic/utils/Sona_BAN.py
@@ -1,6 +1,3 @@
 from pyrogram import filters
 
-def admin_filter():
-    async def func(_, __, message):
-        return bool(message.from_user and message.from_user.is_admin)
-    return filters.create(func)
+admin_filter = filters.admin
