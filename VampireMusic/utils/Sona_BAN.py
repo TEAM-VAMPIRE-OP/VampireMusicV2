4f7b6d78c58f3885e157cb3fc0188ee7932b9a1c
ᯓ꯭𓆰꯭×꯭🔥⁠⁠⁠𝐕꯭𝐀꯭𝐌꯭𝐏꯭𝐈꯭𝐑꯭𝐄꯭ 𝐊꯭𝐈꯭𝐍꯭𝐆 ꯭🌾꯭ <opvampire57@gmail.com>
2025-12-25 19:44:00 +0530

    Update Sona_BAN.py

--- VampireMusic/utils/Sona_BAN.py
+++ VampireMusic/utils/Sona_BAN.py
@@ -1,12 +1,12 @@
 from pyrogram import filters
 
-def admin_filter(_, __, message):
+async def _admin_check(_, __, message):
     if not message.from_user:
         return False
     try:
-        member = message.chat.get_member(message.from_user.id)
+        member = await message.chat.get_member(message.from_user.id)
         return member.status in ("administrator", "creator")
     except:
         return False
 
-admin_filter = filters.create(admin_filter)
+admin_filter = filters.create(_admin_check)
